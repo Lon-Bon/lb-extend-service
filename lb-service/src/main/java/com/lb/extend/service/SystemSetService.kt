584dcc93cf158e2841e0c8c998fc6a2c31c343d0
zhanggaoming <2414807820@qq.com>
2021-09-02 17:49:42 +0800

    Initial submission

---
+++ lb-service/src/main/java/com/lb/extend/service/SystemSetService.kt
@@ -0,0 +1,12 @@
+package com.lb.extend.service
+
+import com.zclever.ipc.annotation.BindImpl
+
+/**
+ * 提供系统设置相关的服务
+ */
+//@BindImpl("com.lonbon.lonbon_app.xxx"),这里要使用BindImpl注解
+interface SystemSetService {
+
+
+}
