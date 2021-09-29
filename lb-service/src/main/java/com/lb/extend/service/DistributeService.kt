c767120eba3b2a69a213c6dc16088f0487d5a4f4
wangjingbo5201314 <wjb5201314>
2021-09-29 17:01:11 +0800

    加入信息发布双屏异显接口

---
+++ lb-service/src/main/java/com/lb/extend/service/DistributeService.kt
@@ -0,0 +1,18 @@
+package com.lb.extend.service
+
+import com.zclever.ipc.annotation.BindImpl
+
+/**
+ * 信息发布双屏异显
+ */
+@BindImpl("com.provider.distribute.DistributeProviderManager")
+interface DistributeService {
+
+
+    /**
+     * 开始双屏异显
+     */
+    fun start();
+
+
+}
