0b2fdeee22f3aa38d114949e934ce69bc40e82f0
wangjingbo5201314 <wjb5201314>
2021-10-11 10:34:15 +0800

    加入信息发布初始化方法

--- lb-service/src/main/java/com/lb/extend/service/DistributeService.kt
+++ lb-service/src/main/java/com/lb/extend/service/DistributeService.kt
@@ -8,11 +8,12 @@ import com.zclever.ipc.annotation.BindImpl
 @BindImpl("com.provider.distribute.DistributeProviderManager")
 interface DistributeService {
 
-    /**
-     * 开始双屏异显
-     */
-    fun start();
+    //初始化信息发布
+    fun init();
 
+
+    // 启动双屏异显（初始化后才能启动）
+    fun start();
 
 
 }
