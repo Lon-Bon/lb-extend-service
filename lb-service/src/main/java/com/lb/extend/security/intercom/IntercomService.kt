01f63af016b7e0b0f437ecf8ade6811315a4ed23
Lon-Bon <Lon-Bon@lonbon.com>
2022-04-02 14:24:52 +0800

    增加安防包名以及IntercomService和SystemSetting接口

---
+++ lb-service/src/main/java/com/lb/extend/security/intercom/IntercomService.kt
@@ -0,0 +1,7 @@
+package com.lb.extend.security.intercom
+
+import com.zclever.ipc.annotation.BindImpl
+
+@BindImpl("com.lonbon.intercom_provider.IntercomServiceImpl")
+interface IntercomService {
+}
---
+++ lb-service/src/main/java/com/lb/extend/security/setting/SystemSettingService.kt
@@ -0,0 +1,17 @@
+package com.lb.extend.security.setting
+
+import com.zclever.ipc.annotation.BindImpl
+
+@BindImpl("com.lonbon.setting_provider.SystemSettingImpl")
+interface SystemSettingService {
+
+    /**
+     * 设置系统时间
+     * @time 从1970年算起的毫秒数
+     */
+    fun  setSystemTime(time:Long)
+
+
+
+
+}
