c08d9ef10a7fb008e1e39e7e641b25d2ff018f05
@bruce_bobo <390463560@qq.com>
2022-11-09 14:32:15 +0800

    增加跟电教相关的接口

--- lb-service/src/main/java/com/lb/extend/security/education/EducationService.kt
+++ lb-service/src/main/java/com/lb/extend/security/education/EducationService.kt
@@ -8,38 +8,19 @@ import com.zclever.ipc.annotation.BindImpl
 interface EducationService {
 
     /**
-     * 初始化电教相关配置，启动前必须先进行初始化
-     *
-     * sn：设备序列号
-     * ip：设备ip
-     * mac：设备mac地址
-     * model：设备型号
-     * clientType：设备类型
-     * clientVersion：设备软件版本号
-     * spaceTotal：总可用空间
-     * spaceFree：剩余可用空间
-     * description：描述信息
+     * 启动前必须先进行初始化
      */
-    fun initEducation(
-        sn: String,
-        ip: String,
-        mac: String,
-        model: String,
-        clientType: String,
-        clientVersion: String,
-        spaceTotal: String,
-        spaceFree: String,
-        description: String,
-    )
+    fun initEducation()
+
 
     /**
      * 跳转到电教点播直播首页Activity
      */
-    fun startEducationMainActivity(context: Context)
+    fun startHomeActivity(context: Context)
 
 
     /**
-     * 退出电教库，点播直播，双屏异显（其实本质就是finish电教Activity，销毁跟电教相关的任何事情）
+     * 退出电教页面
      */
     fun exitEducation()
 
