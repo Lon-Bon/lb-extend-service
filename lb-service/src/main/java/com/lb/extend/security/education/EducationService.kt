1479bbb8969b090d18765797b6c831c009bec543
@bruce_bobo <390463560@qq.com>
2022-11-11 10:55:07 +0800

    规范电教接口

--- lb-service/src/main/java/com/lb/extend/security/education/EducationService.kt
+++ lb-service/src/main/java/com/lb/extend/security/education/EducationService.kt
@@ -1,6 +1,5 @@
 package com.lb.extend.security.education
 
-import android.content.Context
 import com.zclever.ipc.annotation.BindImpl
 
 
@@ -10,18 +9,18 @@ interface EducationService {
     /**
      * 启动前必须先进行初始化
      */
-    fun initEducation()
+    fun init()
 
 
     /**
      * 跳转到电教点播直播首页Activity
      */
-    fun startHomeActivity(context: Context)
+    fun startActivity()
 
 
     /**
      * 退出电教页面
      */
-    fun exitEducation()
+    fun exit()
 
 }
