215d3862cf69490bfcdfa049497626b3de4f7d0d
lbnongxiangang <3435471757@qq.com>
2022-12-28 16:32:10 +0800

    按照张工之前的接口文档进行修改

--- lb-service/src/main/java/com/lb/extend/security/videovisit/VideoVisitSlaveService.kt
+++ lb-service/src/main/java/com/lb/extend/security/videovisit/VideoVisitSlaveService.kt
@@ -1,6 +1,7 @@
 package com.lb.extend.security.videovisit
 
 import com.zclever.ipc.annotation.BindImpl
+import com.zclever.ipc.core.Result
 
 /**
  * 视频会见分机接口
@@ -24,6 +25,36 @@ interface VideoVisitSlaveService {
     fun hangup()
 
     /**
+     * 设置回铃回调，一般呼入或者呼出时会回调
+     * @param callBack Result<VideoVisitClient>
+     */
+    fun setRingBackCallBack(callBack: Result<VideoVisitClient>)
+
+    /**
+     * 设置设备状态改变回调
+     * @param callBack Result<Int>
+     */
+    fun setStateChangeCallBack(callBack: Result<Int>)
+
+    /**
+     * 发送文字通话对象：通话中状态可用
+     * @param message String 限制500个字节，自己定协议
+     */
+    fun sendMessage(message: String)
+
+    /**
+     * 消息接收回调
+     * @param callBack Result<String>
+     */
+    fun setMessageReceiveCallBack(callBack: Result<String>)
+
+    /**
+     * 设置通话时长改变回调（一秒回调一次）
+     * @param callBack Result<Int>
+     */
+    fun setTalkTimeChangeCallBack(callBack: Result<Int>)
+
+    /**
      * 获取当前设备状态
      * @return Int 0：离线，1:空闲，2：呼叫中，3：呼入等待接听中，4：通话中
      */
@@ -36,13 +67,13 @@ interface VideoVisitSlaveService {
     fun getTalkState(): Int
 
     /**
-     * 是否繁忙
+     * 是否繁忙：state>=2
      * @return Int
      */
     fun isBusy(): Int
 
     /**
-     * 是否空闲
+     * 是否空闲：state是否为1
      * @return Int
      */
     fun isIdle(): Int
