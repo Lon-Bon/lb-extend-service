a2820d5f34cfa15e830766b562d1f0cc6b86bfa9
baohekang <1357185257@qq.com>
2024-06-04 11:09:23 +0800

    医院悬浮窗对讲服务添加视频对讲相关接口

--- lb-service/src/main/java/com/lb/extend/service/TalkService.kt
+++ lb-service/src/main/java/com/lb/extend/service/TalkService.kt
@@ -38,6 +38,58 @@ interface TalkService {
      * 接听
      * **/
     fun  answer()
+
+    /**
+     * 开关视频模式
+     * */
+    fun setVideoMode(open:Boolean)
+
+    /**
+     * 设置本地视频悬浮窗位置以及大小
+     * */
+    /**
+     * 设置本地预览页面显示位置（单位px）
+     * @param left 视频画面离屏幕左间距
+     * @param top 视频画面离屏幕上间距
+     * @param width 视频画面宽
+     * @param height 视频画面高
+     */
+    fun setPreViewPosition(left: Int, top: Int, width: Int, height: Int)
+
+
+    /**
+     * 设置对方视频悬浮窗位置以及大小
+     * */
+    /**
+     * 设置对方预览页面显示位置（单位px）
+     * @param left 视频画面离屏幕左间距
+     * @param top 视频画面离屏幕上间距
+     * @param width 视频画面宽
+     * @param height 视频画面高
+     */
+    fun setRemoteViewPosition(left: Int, top: Int, width: Int, height: Int)
+
+    /**
+     * 隐藏本地视频画面
+     * */
+    fun hideLocalView()
+
+
+    /**
+     * 隐藏对方视频画面
+     * */
+    fun hideRemoteView()
+
+    /**
+     * 设置本地视频画面全屏，true 全屏，false 取消全屏
+     * */
+    fun setLocalViewFullScreen(full:Boolean)
+
+    /**
+     * 设置对方视频画面全屏，true 全屏，false 取消全屏
+     * */
+    fun setRemoteViewFullScreen(full: Boolean)
+
 }
 
 
