9f24b5552f6331d6f58b8cb48c45005f1a4cb6c1
zhw <zhw@qq.com>
2024-11-13 13:47:01 +0800

    feat(FaceApiService):修改人脸和指纹检测接口

--- lb-service/src/main/java/com/lb/extend/security/fingerprint/FingerprintService.kt
+++ lb-service/src/main/java/com/lb/extend/security/fingerprint/FingerprintService.kt
@@ -79,15 +79,18 @@ interface FingerprintService {
      * 清空本地所有指纹存储信息
      */
     fun clearAllFingerprint()
-
     /**
      * 指纹特征值创建接口
      * @param img 指纹图片数据
      * @param feature 根据图片数据生成的指纹特征结果
      * @return 0成功，1设备未打开，2生成失败
      */
-    fun fingerFeatureCreat(img: ByteArray, feature: ByteArray): Int
+    fun fingerFeatureCreat(img: ByteArray, feature: ByteArray): ByteArray?
 
+    /**
+     * 回调指纹特征值创建
+     */
+    fun onFingerFeatureCreat(callBack: Result<ByteArray>)
     /**
      * 根据本地指纹图片注册指纹
      * 传入一张指纹图片路径，注册指纹
@@ -95,8 +98,11 @@ interface FingerprintService {
      * @param feature 指纹特征值提取返回结果，直接传new byte[512]
      * @return  0 成功，1 设备未打开，2 提取失败，3其他
      */
-    fun autoRegisterFromFile(path: String, feature: ByteArray): Int
-
+    fun autoRegisterFromFile(path: String, feature: ByteArray): ByteArray?
+    /**
+     * 回调图片注册指纹
+     */
+    fun onAutoRegisterFromFile(callBack: Result<ByteArray>)
     /**
      * 指纹1：1比对接口
      * 指纹识别
@@ -105,9 +111,12 @@ interface FingerprintService {
      * @param similarity 相似度
      * @return 0 成功，1 设备未打开，2 未注册，3识别失败
      */
-    fun fringerCompare(path1: String, path2: String, similarity: IntArray): Int
+    fun fringerCompare(path1: String, path2: String, similarity: IntArray): Float
 
-
+    /**
+     * 回调指纹1：1比对接口
+     */
+    fun onFringerCompare(callBack: Result<Float>)
 
     /**
      *  指纹探测
@@ -115,7 +124,10 @@ interface FingerprintService {
      *  @return 0未按压指纹，1设备未打开，2检测到指纹按压
      */
     fun  isFingerPress():Int
-
+    /**
+     * 回调指纹1：1比对接口
+     */
+    fun onIsFingerPress(callBack: Result<Int>)
 }
 
 /**
